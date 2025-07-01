5dc48756f729f6e6b114233d5e95763bd6e2abcb
Talic72 <EMORRISWORK72@GMAIL.COM>
2025-06-30 22:21:25 -0400

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Objects;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter your party affiliation in the form of R, D, OR I:");
+        String inputChoice = in.nextLine();
+
+        if(Objects.equals(inputChoice, "D")){
+            System.out.println(" You get a democratic Donkey!");
+        }
+        else if(Objects.equals(inputChoice, "R")) {
+            System.out.println(" You get a Republican elephant!");
+        }
+        else if(Objects.equals(inputChoice, "I")) {
+            System.out.println(" You get a Independent Iguana!");
+        }
+        else {
+            System.out.println(" You get the Other otter!");
         }
     }
 }
+
+
